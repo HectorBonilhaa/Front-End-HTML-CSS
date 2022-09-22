b75f4d1d1dea68c86e6b0f620250fc62d6553d9e
HectorBonilhaa <hectoroliveira1@gmail.com>
2022-09-22 18:07:52 -0300

    Aula Front End 2022

---
+++ Aula3/Js/aula1.js
@@ -0,0 +1,29 @@
+//Comentário
+/*
+
+*/
+//Números
+10;
+10.5;
+//Boolean
+true
+false
+
+//array
+[10, 10.9, "oi", true]
+
+//objeto
+var aluno = {
+    nome: "Maria",
+    sobrenome: "Silva"
+};
+console.log(aluno)
+
+// tipagem dinâmica
+var y = 15;
+y = "oi";
+
+//variável undefined
+var y;
+var aluno = null;
+
